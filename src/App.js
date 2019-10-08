af6f603a5f7f1c1c8e7b495da4ecd0b870e8d561
Crixin <47997631+Crixin@users.noreply.github.com>
2019-10-08 20:44:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import { Input } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
 
 function App() {
   
   const [posts, setPosts] = useState([])
-  
-/*   useEffect(()=>{
-    fetch(`http://www.omdbapi.com/?s=marvel&apikey=54a10c8c`)
-    .then(response => response.json())
-    .then(json => setPosts(json.Search))
-  },[]);
-   */
+  const [favoritos, setfavoritos] = useState([])
+
+  const [likes, setLikes] = useState(0);
+
+  useEffect(() => {
+    const curtidas = posts.filter(post => post.like).length;
+    setLikes(curtidas);
+  }, [posts])
+
   const buscaFilmes = () =>{
     let busca = document.getElementById("titulo").value;
-    console.log(busca)
-     fetch(`http://www.omdbapi.com/?s=${busca}&apikey=54a10c8c`)
+    fetch(`http://www.omdbapi.com/?s=${busca}&apikey=54a10c8c`)
     .then(response => response.json())
     .then(json => setPosts(json.Search))
   };
   
+  const handleClick = (id) => {
+    const novosPosts = posts.map(post => {
+      if(post.imdbID === id){
+
+        if(!post.like) {
+          favoritos.push(post) 
+          console.log(favoritos)
+        } else{
+          var index = -1;
+          favoritos.map(function(el, i){
+            if(el.imdbID == post.imdbID) {
+              index = i;
+            }
+          });
+          favoritos.splice(index, 1);
+        }
+      }
+      return post.imdbID === id ? {...post, like: !post.like} : post;
+    });
+
+    setPosts(novosPosts);
+  }
+  
+  const addFav = (id) => {
+    setfavoritos(id);
+  }
   
   return (
     <>
-    <input id="titulo"></input>
-    <button onClick={ () => buscaFilmes() }>Busca</button>
-    <ul>
-      { posts && posts.map((post, idx )=>{
-        return <li key={idx} >
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Input id="titulo"></Input>
+        <Button variant="contained" color="primary"  onClick={ () => buscaFilmes() }>
+        Busca
+        </Button>
+        <Button variant="contained" color="secondary">Favoritos</Button>
+
+        <ul>
+        { posts && posts.map(post =>{
+          return <li key={post.imdbID} >
           Title: {post.Title}<br/>
           Ano lançamento: {post.Year}<br/>
           <img src={post.Poster}></img><br></br>
+          {post.like && 
+            <span>Favoritado!</span>
+          }
+          <Button variant="contained" color="secondary" onClick={(e) => handleClick(post.imdbID)}> Favoritar </Button>
           </li>
-
-      }) }
-    </ul>
+          
+        }) 
+        }
+        </ul>
+      </Box>
+    </Container>
     </> 
     );
   }
